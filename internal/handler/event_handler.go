bd6da193f99bb008b49df99454490d1dc98e017f
Finnik <73883383+golkity@users.noreply.github.com>
2025-06-22 19:28:52 +0300

    Fix bugs

--- internal/handler/event_handler.go
+++ internal/handler/event_handler.go
@@ -3,12 +3,12 @@ package http
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"github.com/go-chi/chi/v5"
 	"kulturaGo/events-service/internal/domain"
+	"kulturaGo/events-service/internal/dto"
 	nethttp "net/http"
 	"strconv"
-	"time"
-
-	"github.com/go-chi/chi/v5"
 )
 
 type EventHandler struct {
@@ -24,7 +24,7 @@ func (h *EventHandler) ListActive(w nethttp.ResponseWriter, r *nethttp.Request) 
 	}
 	offset, _ := strconv.Atoi(r.URL.Query().Get("offset"))
 
-	evs, err := h.Repo.ListActive(r.Context(), limit, offset)
+	evs, err := h.Repo.ListPublic(r.Context(), limit, offset)
 	if err != nil {
 		nethttp.Error(w, err.Error(), 500)
 		return
@@ -34,7 +34,7 @@ func (h *EventHandler) ListActive(w nethttp.ResponseWriter, r *nethttp.Request) 
 
 func (h *EventHandler) GetBySlug(w nethttp.ResponseWriter, r *nethttp.Request) {
 	slug := chi.URLParam(r, "slug")
-	ev, err := h.Repo.GetBySlug(r.Context(), slug)
+	ev, err := h.Repo.GetPublicBySlug(r.Context(), slug)
 	if err != nil {
 		nethttp.NotFound(w, r)
 		return
@@ -48,33 +48,24 @@ func writeJSON(_ context.Context, w nethttp.ResponseWriter, v any) {
 }
 
 func (h *EventHandler) Create(w nethttp.ResponseWriter, r *nethttp.Request) {
-	var in struct {
-		Title       string    `json:"title"`
-		CategoryID  int16     `json:"category_id"`
-		Description string    `json:"description"`
-		PlaceID     int64     `json:"place_id"`
-		StartsAt    time.Time `json:"starts_at"`
-		EndsAt      time.Time `json:"ends_at"`
-	}
-	if err := json.NewDecoder(r.Body).Decode(&in); err != nil {
+	ctx := r.Context()
+	var input dto.PublicEvent
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
 		nethttp.Error(w, err.Error(), nethttp.StatusBadRequest)
 		return
 	}
 
-	ev := domain.Event{
-		Title:       in.Title,
-		CategoryID:  in.CategoryID,
-		Description: in.Description,
-		PlaceID:     in.PlaceID,
-		StartsAt:    in.StartsAt,
-		EndsAt:      in.EndsAt,
-	}
-	if err := h.Repo.Create(r.Context(), &ev); err != nil {
+	pubEvent, err := h.Repo.Create(ctx, input)
+	if err != nil {
 		nethttp.Error(w, err.Error(), nethttp.StatusInternalServerError)
 		return
 	}
 
-	w.Header().Set("Location", "/api/v1/events/"+ev.Slug)
+	location := fmt.Sprintf("/api/v1/events/%s", pubEvent.Slug)
+	w.Header().Set("Location", location)
+
 	w.WriteHeader(nethttp.StatusCreated)
-	writeJSON(r.Context(), w, ev)
+	if err := json.NewEncoder(w).Encode(pubEvent); err != nil {
+		nethttp.Error(w, err.Error(), nethttp.StatusInternalServerError)
+	}
 }
